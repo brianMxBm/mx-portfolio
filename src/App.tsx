8564d754b73a19d79f4ffceb13778fdc87a647b6
brianMxBm <brian.melgar.mx@gmail.com>
2025-08-21 16:26:54 -0700

    Initial Vite Project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-beige-500">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default {
+  plugins: [react(), tailwindcss()],
+};
